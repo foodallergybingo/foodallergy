8820a7de097033aa195e56c8f6f093e78f60898f
foodallergybingo <Ghttoetiaak19$93>
2017-10-09 21:54:43 +0200

    Test

--- bingo/flask_app.py
+++ bingo/flask_app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder = 'build')
+app = Flask(__name__, template_folder = 'build', static_folder='build/static')
 
 @app.route('/')
 def hello_world():
