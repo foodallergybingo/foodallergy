06f2f454861aade023e7835a3803049cbedcf6af
foodallergybingo <Ghttoetiaak19$93>
2017-10-09 21:33:56 +0200

    Test

--- bingo/flask_app.py
+++ bingo/flask_app.py
@@ -3,9 +3,9 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'build')
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', template_folder = 'build')
+    return render_template('index.html')
 
